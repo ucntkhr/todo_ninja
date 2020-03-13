09516ad86f533afa7df6257cedc3734273703932
ucntkhr <ucntkhr.gftdwks@gmail.com>
2020-03-13 11:48:13 +0900

    Button Tutorial

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,9 +6,20 @@
     <v-btn depressed dark color="pink" class="mr-2">click me</v-btn>
     <v-btn text color="pink">click me</v-btn>
 
-    <v-btn depressed class="pink white--text">
-      <v-icon>email</v-icon>
+    <v-btn depressed class="pink white--text mr-2">
+      <v-icon left>mdi-email</v-icon>
+      <span>email me</span>
     </v-btn>
+
+    <v-btn depressed small class="pink white--text mr-2">
+      <v-icon left small>mdi-email</v-icon>
+      <span>email me</span>
+    </v-btn>
+
+    <v-btn fab depressed small dark color="red">
+      <v-icon small>mdi-heart</v-icon>
+    </v-btn>
+    
 
   </div>
 </template>
